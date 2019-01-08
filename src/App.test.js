a7a9ea719e7572357b69d50b3c8f9813ef7a3a90
Stanislav Aituhanov <aytuganov@gmail.com>
2019-01-08 23:40:21 +0200

    Flop. Test initial state  & deal  button

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
 import { shallow } from 'enzyme';
 
 const app = shallow(<App />);
-
-it('renders without crashing', () => {
-  expect(app).toMatchSnapshot();
-  
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    expect(app).toMatchSnapshot();
+  });
+})
--- src/components/Game/Flop/Flop.tsx
+++ src/components/Game/Flop/Flop.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { Component } from 'react';
 
-import Card from './Card/Card';
-
 import { isMobile } from 'react-device-detect';
 import { joinCss } from '~/utils';
 
 import * as css from './Flop.scss';
 import SvgFlop from '../SvgFlop/SvgFlop';
 
-const flopCards = [
+import Card from '~/models/card';
+
+const flopCards: Array<Card> = [
   {name: "diamond_jack", coord: {x: 0, y: 0}, status: 'Flop_1'},
   {name: "club_jack", coord: {x: 0, y: 0}, status: 'Flop_2'},
   {name: "spade_jack", coord: {x: 0, y: 0}, status: 'Flop_3'},
@@ -21,11 +21,7 @@ const riverCard = {name: "club_9", coord: {x: 0, y: 0}, status: 'River'};
 class Flop extends Component<any, any> {
   constructor(props) {
     super(props);
-    this.state = {
-      flop: undefined,
-      turn: undefined,
-      river:undefined,
-    }
+    this.state = {};
   }
 
   dealCards = () => {
@@ -43,7 +39,7 @@ class Flop extends Component<any, any> {
     const { flop, turn, river} = this.state;
     return (
       <div className={joinCss(css.Flop, isMobile ? css.Flop_Mobile: css.Flop_Browser)}>
-      <button onClick={this.dealCards}>Deal</button>
+      <button className="deal" onClick={this.dealCards}>Deal</button>
         <div className={css.Flop_Label}>{this.props.label}</div>
         <SvgFlop flop={flop} turn={turn} river={river}/>
       </div>
