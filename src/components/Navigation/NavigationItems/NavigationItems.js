1bdb6afaf57cdbc217de445d98dcbdd16d31b537
Stanislav Aituhanov <aytuganov@gmail.com>
2019-01-19 10:07:45 +0200

    Code refactoring

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,44 +3,29 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
+const renderAuthenticated = () => (
+  <React.Fragment>
+    <NavigationItem link="/real-money" exact>Real Money</NavigationItem>
+    <NavigationItem link="/fast-forward" exact>Fast Forward</NavigationItem>
+    <NavigationItem link="/tournaments" exact>Tournaments</NavigationItem>
+    <NavigationItem link="/sit-and-go" exact>Sit and Go</NavigationItem>
+    <NavigationItem link="/my-tournaments" exact>My Tournaments</NavigationItem>
+    <NavigationItem link="/casino" exact>Casino</NavigationItem>
+    <NavigationItem link="/chat" exact>Chat</NavigationItem>
+    <NavigationItem link="/game">Game</NavigationItem>
+    <NavigationItem link="/logout">Logout</NavigationItem>
+  </React.Fragment>
+  );
+
+const renderShared = () => (
+  <NavigationItem link="/auth">Authenticate</NavigationItem>
+)
+
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
-    {props.isAuthenticated ? (
-      <NavigationItem link="/real-money" exact>
-        Real Money
-      </NavigationItem>
-    ) : null}
-    {props.isAuthenticated ? (
-      <NavigationItem link="/fast-forward" exact>
-        Fast Forward
-      </NavigationItem>
-    ) : null}
-    {props.isAuthenticated ? (
-      <NavigationItem link="/tournaments" exact>
-        Tournaments
-      </NavigationItem>
-    ) : null}
-    {props.isAuthenticated ? (
-      <NavigationItem link="/sit-and-go" exact>
-        Sit and Go
-      </NavigationItem>
-    ) : null}
-    {props.isAuthenticated ? (
-      <NavigationItem link="/my-tournaments" exact>
-        My Tournaments
-      </NavigationItem>
-    ) : null}
-    {props.isAuthenticated ? (
-      <NavigationItem link="/casino" exact>
-        Casino
-      </NavigationItem>
-    ) : null}
-    {props.isAuthenticated ? <NavigationItem link="/game">Game</NavigationItem> : null}
-    {!props.isAuthenticated ? (
-      <NavigationItem link="/auth">Authenticate</NavigationItem>
-    ) : (
-      <NavigationItem link="/logout">Logout</NavigationItem>
-    )}
+    {
+      props.isAuthenticated ? renderAuthenticated() : renderShared()
+    }
   </ul>
 );
 
